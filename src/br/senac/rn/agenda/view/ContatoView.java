32399a837455d696ab4b90422d7ea3d902a56fcc
Weskley Bezerra <weskleymb@hotmail.com>
2019-08-14 19:51:31 -0300

    adicao da view

--- src/br/senac/rn/agenda/view/ContatoView.java
+++ src/br/senac/rn/agenda/view/ContatoView.java
@@ -1,4 +1,32 @@
 package br.senac.rn.agenda.view;
 
+import java.util.Scanner;
+
 public class ContatoView {
+
+    private static Scanner entrada = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Long escolha = 1L;
+
+        do {
+            ContatoView.getMenu();
+            escolha = entrada.nextLong();
+        } while (escolha != 0);
+
+    }
+
+    private static void getMenu() {
+        String menu = "";
+        menu += "=============================== \n";
+        menu += "1 - Cadastrar Novo Contato      \n";
+        menu += "2 - Listar Todos os Contatos    \n";
+        menu += "3 - Buscar Contato              \n";
+        menu += "0 - SAIR                        \n";
+        menu += "=============================== \n";
+        menu += "Opção: ";
+        System.out.print(menu);
+    }
+
 }
